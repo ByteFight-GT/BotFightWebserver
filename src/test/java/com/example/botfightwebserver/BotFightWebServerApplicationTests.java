35c40b9a5877a6aea8c64c5d8d5668a71d33a44a
Tkwok7 <tkwok7@gatech.edu>
2024-11-10 16:15:02 -0500

    build working

--- src/test/java/com/example/botfightwebserver/BotFightWebServerApplicationTests.java
+++ src/test/java/com/example/botfightwebserver/BotFightWebServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.botfightwebserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@PostgresIntegrationTest
 class BotFightWebServerApplicationTests {
 
     @Test
--- src/test/java/com/example/botfightwebserver/player/PlayerRepositoryTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.botfightwebserver.player;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class PlayerRepositoryTest {
-
-}
