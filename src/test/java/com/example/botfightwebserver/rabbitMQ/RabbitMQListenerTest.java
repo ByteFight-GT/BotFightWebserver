2842c2df2a993b9604f6faf1a0a9f721746a56cc
Tkwok7 <tkwok7@gatech.edu>
2024-11-16 22:15:39 -0500

    rabbit tests

--- src/test/java/com/example/botfightwebserver/rabbitMQ/RabbitMQListenerTest.java
+++ src/test/java/com/example/botfightwebserver/rabbitMQ/RabbitMQListenerTest.java
@@ -1,7 +1,37 @@
 package com.example.botfightwebserver.rabbitMQ;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.botfightwebserver.gameMatch.GameMatchResult;
+import com.example.botfightwebserver.gameMatch.GameMatchResultHandler;
+import com.example.botfightwebserver.gameMatch.MATCH_STATUS;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class RabbitMQListenerTest {
 
+    @Mock
+    private GameMatchResultHandler gameMatchResultHandler;
+
+    private RabbitMQListener rabbitMQListener;
+
+    @BeforeEach
+    void setUp() {
+        rabbitMQListener = new RabbitMQListener(gameMatchResultHandler);
+    }
+
+    @Test
+    void receiveGameMatchResults_ShouldProcessMessageSuccessfully() {
+        GameMatchResult gameMatchResult = new GameMatchResult(1L, MATCH_STATUS.PLAYER_TWO_WIN, "SOME LOGS");
+
+        rabbitMQListener.receiveGameMatchResults(gameMatchResult);
+
+        verify(gameMatchResultHandler, times(1)).handleGameMatchResult(gameMatchResult);
+    }
 }
