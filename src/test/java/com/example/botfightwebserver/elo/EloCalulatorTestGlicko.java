648bbf5669c4abb284d32058d0bf5f8405d4339a
patrick hansen <143661314+pmhansen8@users.noreply.github.com>
2024-11-21 21:41:45 -0500

    Update and rename EloCalulatorTest to EloCalulatorTestGlicko

--- src/test/java/com/example/botfightwebserver/elo/EloCalulatorTestGlicko.java
+++ src/test/java/com/example/botfightwebserver/elo/EloCalulatorTestGlicko.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class EloCalculatorTest {
+class EloCalculatorTestGlicko {
 
     @Test
     void testCalculateEloPlayerOneWins() {
